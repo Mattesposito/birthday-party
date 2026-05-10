961a63ce87a2d4f81a46d55dac00531d351391df
Mattesposito <133373399+Mattesposito@users.noreply.github.com>
2026-05-10 16:14:39 +0200

    Update Supabase configuration with new credentials

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "https://dbtwqhjabanepxrdslyk.supabase.co",
-  publishableKey: "sb_publishable_lksqKA32qAc38BATigfB9g_-xU7mR43"
+  url: "https://vvocwqkcexboolhpphcy.supabase.co",
+  publishableKey: "sb_publishable_TiPdLKIwuv9HRUTiiApz9A_VJI-Esil"
 };
